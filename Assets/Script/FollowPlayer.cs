d93157109e1883e9fedee1d8778b32659df2e838
negset <negset1858@gmail.com>
2018-11-03 03:38:09 +0900

    Initial commit

---
+++ Assets/Script/FollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform target;
+    private Vector3 offset;
+
+    void Start()
+    {
+        // 自分とtargetとの相対座標を求める.
+        offset = GetComponent<Transform>().position - target.position;
+    }
+
+    void Update()
+    {
+        // 自分の座標に(targetの座標 + offfset)を代入する.
+        GetComponent<Transform>().position = target.position + offset;
+    }
+}
